51d78235e8b40c858873c1df00a140789817a4d6
jeremie <permentierj@gmail.com>
2022-07-19 22:26:19 +0200

    Ajout de la gestion des menus

--- starter/functions.php
+++ starter/functions.php
@@ -1,4 +1,24 @@
 <?php
 
+/**
+ * Active la prise en charge des images mises en avant sur les publications et les articles.
+ * Enables support for featured images on posts and articles.
+ */
 add_theme_support('post-thumnails');
+
+/**
+ * Active la prise en charge des titres dynamiques.
+ * Enables support for dynamic titles
+ */
 add_theme_support('title-tag');
+
+/**
+ * Activer la gestion des menus.
+ * Enable menu management.
+ */
+register_nav_menus(
+    array(
+        'primary' => esc_html__('Menu principal', 'starter'),
+        'secondary' => __('Menu secondaire', 'starter'),
+    )
+);
