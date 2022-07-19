04bc698197cfc4adbb23653c8404e89d526781a2
jeremie <permentierj@gmail.com>
2022-07-19 21:32:00 +0200

    Modification des fichiers home.php et archive.php

--- starter/archive.php
+++ starter/archive.php
@@ -1,5 +1,19 @@
 <?php get_header(); ?>
-
+<?php 
+    if ( is_category() ) {
+        $title = "Catégorie : " . single_tag_title( '', false );
+    }
+    elseif ( is_tag() ) {
+        $title = "Étiquette : " . single_tag_title( '', false );
+    }
+    elseif ( is_search() ) {
+        $title = "Vous avez recherché : " . get_search_query();
+    }
+    else {
+        $title = 'Le Blog';
+    }
+?>
+<h1><?php echo $title; ?></h1>
 	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
   
 		<article class="post">
